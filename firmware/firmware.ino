90104c00469f23907d97b03f1056085cf10e2623
pashamray <pashamray@gmail.com>
2025-01-26 13:47:21 +0100

    add command set speed

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,3 +1,5 @@
+unsigned int speed = 0;
+
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);
   Serial.begin(9600);
@@ -14,6 +16,15 @@ void loop() {
     
     if (command.equals("ping")) {
       Serial.println("pong");
+    }
+
+    if (command.startsWith("set")) {
+      if (command.startsWith("speed", 4)) {
+        speed = command.substring(9).toInt();
+
+        Serial.printf("set speed: %d", speed);
+        Serial.println();
+      }
     }
   }
 }
