67a05cb695e95fdbf2083825020f82f54e1240d2
pashamray <pashamray@gmail.com>
2025-01-26 14:05:20 +0100

    change serial receive to onReceive

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -4,6 +4,7 @@ unsigned int timer = 0;
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);
   Serial.begin(9600);
+  Serial.onReceive(serialReceive);
 }
 
 void loop() {
@@ -11,40 +12,44 @@ void loop() {
   delay(500);
   digitalWrite(LED_BUILTIN, LOW);
   delay(500);
+}
 
-  if (Serial.available()) {
-    String command = Serial.readStringUntil('\n');
-    
-    if (command.equals("ping")) {
-      Serial.println("pong");
+void serialReceive() {
+  if (Serial.available() == 0) {
+    return;
+  }
+
+  String command = Serial.readStringUntil('\n');
+  
+  if (command.equals("ping")) {
+    Serial.println("pong");
+  }
+
+  if (command.startsWith("set")) {
+    if (command.startsWith("speed", 4)) {
+      speed = command.substring(9).toInt();
+
+      Serial.printf("set speed: %d", speed);
+      Serial.println();
     }
 
-    if (command.startsWith("set")) {
-      if (command.startsWith("speed", 4)) {
-        speed = command.substring(9).toInt();
+    if (command.startsWith("timer", 4)) {
+      timer = command.substring(9).toInt();
 
-        Serial.printf("set speed: %d", speed);
-        Serial.println();
-      }
+      Serial.printf("set timer: %d", timer);
+      Serial.println();
+    }
+  }
 
-      if (command.startsWith("timer", 4)) {
-        timer = command.substring(9).toInt();
-
-        Serial.printf("set timer: %d", timer);
-        Serial.println();
-      }
+  if (command.startsWith("get")) {
+    if (command.startsWith("speed", 4)) {
+      Serial.printf("get speed: %d", speed);
+      Serial.println();
     }
 
-    if (command.startsWith("get")) {
-      if (command.startsWith("speed", 4)) {
-        Serial.printf("get speed: %d", speed);
-        Serial.println();
-      }
-
-      if (command.startsWith("timer", 4)) {
-        Serial.printf("get timer: %d", timer);
-        Serial.println();
-      }
+    if (command.startsWith("timer", 4)) {
+      Serial.printf("get timer: %d", timer);
+      Serial.println();
     }
   }
 }
